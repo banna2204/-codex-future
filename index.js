635ecd89a2ab7093268717d8a900cb39f675be61
shubham singh <student@gmail.com>
2026-03-10 15:59:24 +0530

    route handle

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 import express from 'express';
 import cors from 'cors';
-import path from "path";
 import 'dotenv/config'
-
 import mailRouter from './route/mailroute.js'
 import courseRouter from './route/courseRoute.js'
 import feedbackRouter from './route/feedbackRoute.js'
 import addressRouter from './route/addressRoute.js'
 import placedRouter from './route/placedRoute.js'
-import adminRouter from './route/adminroute.js'
-
 import connectDB from './config/db.js'
+import adminRouter from './route/adminroute.js'
+import path from 'path';
 
 const app = express();
-
 app.use(express.json());
-
 app.use(cors({
-  origin: '*',
+  origin: '*',     
   credentials: true
-}));
-
+}))
+const PORT = 5000;
 await connectDB();
+
+
+app.get('/',(_,res)=>{
+  res.send('hello');
+})
 
 app.use('/api/user',mailRouter);
 app.use('/api/course',courseRouter);
@@ -33,15 +34,12 @@ app.use('/api/address',addressRouter);
 app.use('/api/placed',placedRouter);
 app.use('/api/admin',adminRouter);
 
-
 app.use(express.static(path.join(process.cwd(), "build")));
 
 app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(process.cwd(), "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT,()=>{
   console.log('server on port',PORT);
-});
+})
